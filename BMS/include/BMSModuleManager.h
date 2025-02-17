4b4292a5665880013c33a20dd6b44e445489bc27
hvguy0 <47881477+hvguy0@users.noreply.github.com>
2025-02-16 22:30:57 -0600

    Add files via upload

---
+++ BMS/include/BMSModuleManager.h
@@ -0,0 +1,72 @@
+#pragma once
+#include "config.h"
+#include "BMSModule.h"
+
+class BMSModuleManager
+{
+  public:
+    BMSModuleManager();
+    int seriescells();
+    void clearmodules();
+    void StopBalancing();
+    void balanceCells(int duration, int debug);
+    void setupBoards();
+    void findBoards();
+    void renumberBoardIDs();
+    void clearFaults();
+    void sleepBoards();
+    void wakeBoards();
+    void getAllVoltTemp();
+    void readSetpoints();
+    void setBatteryID(int id);
+    void setPstrings(int Pstrings);
+    void setUnderVolt(float newVal);
+    void setOverVolt(float newVal);
+    void setOverTemp(float newVal);
+    void setBalanceV(float newVal);
+    void setBalanceHyst(float newVal);
+    void setSensors(int sensor, float Ignore);
+    float getPackVoltage();
+    float getAvgTemperature();
+    float getHighTemperature();
+    float getLowTemperature();
+    float getAvgCellVolt();
+    float getLowCellVolt();
+    float getHighCellVolt();
+    float getHighVoltage();
+    float getLowVoltage();
+    int getBalancing();
+    uint16_t getcellvolt(int modid, int cellid);
+    uint16_t gettemp(int modid, int tempid);
+    /*
+      void processCANMsg(CAN_FRAME &frame);
+    */
+    void printAllCSV(unsigned long timestamp,float current, int SOC, int delim);
+    void printPackSummary();
+    void printPackDetails(int digits);
+int getNumModules();
+
+  private:
+    float packVolt;                         // All modules added together
+    int Pstring;
+    float LowCellVolt;
+    float HighCellVolt;
+    float lowestPackVolt;
+    float highestPackVolt;
+    float lowestPackTemp;
+    float highestPackTemp;
+    float highTemp;
+    float lowTemp;
+    BMSModule modules[MAX_MODULE_ADDR + 1]; // store data for as many modules as we've configured for.
+    int batteryID;
+    int numFoundModules;                    // The number of modules that seem to exist
+    bool isFaulted;
+    int spack;
+    int CellsBalancing;
+    /*
+      void sendBatterySummary();
+      void sendModuleSummary(int module);
+      void sendCellDetails(int module, int cell);
+    */
+
+};
---
+++ BMS/include/config.h
@@ -0,0 +1,104 @@
+#pragma once
+
+#include <Arduino.h>
+
+//Set to the proper port for your USB connection - SerialUSB on Due (Native) or Serial for Due (Programming) or Teensy
+#define SERIALCONSOLE   Serial
+
+//Define this to be the serial port the Tesla BMS modules are connected to.
+//On the Due you need to use a USART port (Serial1, Serial2, Serial3) and update the call to serialSpecialInit if not Serial1
+//Serial3 for teensy
+#define SERIALBMS  Serial3
+
+#define REG_DEV_STATUS      0
+#define REG_GPAI            1
+#define REG_VCELL1          3
+#define REG_VCELL2          5
+#define REG_VCELL3          7
+#define REG_VCELL4          9
+#define REG_VCELL5          0xB
+#define REG_VCELL6          0xD
+#define REG_TEMPERATURE1    0xF
+#define REG_TEMPERATURE2    0x11
+#define REG_ALERT_STATUS    0x20
+#define REG_FAULT_STATUS    0x21
+#define REG_COV_FAULT       0x22
+#define REG_CUV_FAULT       0x23
+#define REG_ADC_CTRL        0x30
+#define REG_IO_CTRL         0x31
+#define REG_BAL_CTRL        0x32
+#define REG_BAL_TIME        0x33
+#define REG_ADC_CONV        0x34
+#define REG_ADDR_CTRL       0x3B
+
+#define MAX_MODULE_ADDR     0x3E
+
+#define EEPROM_VERSION      0x14    //update any time EEPROM struct below is changed.
+#define EEPROM_PAGE         0
+
+typedef struct {
+  uint8_t version;
+  uint8_t checksum;
+  uint32_t canSpeed;
+  uint8_t batteryID;  //which battery ID should this board associate as on the CAN bus
+  uint8_t logLevel;
+  float OverVSetpoint;
+  float UnderVSetpoint;
+  float DischHys;
+  float ChargeVsetpoint;
+  float DischVsetpoint;
+  float ChargeHys;
+  float StoreVsetpoint;
+  float WarnOff;
+  float OverTSetpoint;
+  float UnderTSetpoint;
+  uint16_t triptime;
+  float ChargeTSetpoint;
+  float DisTSetpoint;
+  float WarnToff;
+  float CellGap;
+  uint8_t IgnoreTemp;
+  float IgnoreVolt;
+  float balanceVoltage;
+  float balanceHyst;
+  int Scells;
+  int Pstrings;
+  int CAP;
+  uint16_t  chargecurrentmax;
+  uint16_t  chargecurrent2max;
+  uint16_t  chargecurrentend;
+  uint16_t  discurrentmax ;
+  int socvolt[4];
+  int invertcur;
+  int cursens;
+  int curcan;
+  int voltsoc;
+  int Pretime;
+  int conthold;
+  int Precurrent;
+  float convhigh;
+  float convlow;
+  int32_t changecur;
+  uint16_t offset1;
+  uint16_t offset2;
+  int balanceDuty;
+  int ESSmode;
+  int gaugelow;
+  int gaugehigh;
+  int ncur;
+  int chargertype;
+  uint32_t chargerspd; //was int
+  uint16_t UnderDur;
+  uint16_t CurDead;
+  float DisTaper;
+  uint8_t ChargerDirect; // bool
+  uint8_t ExpMess; // bool
+  uint8_t SerialCan; // bool
+  uint16_t PulseCh;
+  uint16_t PulseChDur;
+  uint16_t PulseDi;
+  uint16_t PulseDiDur;
+  uint8_t tripcont;
+  int chargereff;
+  int chargerACv;
+} EEPROMSettings;
